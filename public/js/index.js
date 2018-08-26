d400108fea6a6fe0376f11ccd880ff2afbde7189
ostinlviv <oyaremchuk@perpet.io>
2018-08-26 17:42:19 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Ostap',
-        text: 'Hey you'
-    })
 });
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
