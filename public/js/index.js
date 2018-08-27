006cbccad57bd654a6a0d7a87ad3499eb4771b7f
ostinlviv <oyaremchuk@perpet.io>
2018-08-27 19:05:57 +0300

    Add styles

--- public/js/index.js
+++ public/js/index.js
@@ -26,11 +26,14 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
+
+    var messageTextBox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
         text: jQuery('[name=message]').val()
     }, function () {
-
+        messageTextBox.val('');
     });
 });
 
@@ -47,7 +50,7 @@ jQuery('[name=message]').keyup(function() {
 
 socket.on('setMessage', function(status){
     if (status.isTyping) {
-        jQuery('#status').text('typing')
+        jQuery('#status').text('Someone typing...')
     } else {
         jQuery('#status').empty()
     }
@@ -58,12 +61,17 @@ locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.');
     }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
