da70011a5cf5c27afb0548c1aa06fdcd3521b959
ostinlviv <oyaremchuk@perpet.io>
2018-08-27 14:06:38 +0300

    Add geolocation and typing features

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
 var socket = io();
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
+
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
@@ -13,6 +15,15 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
     socket.emit('createMessage', {
@@ -21,4 +32,38 @@ jQuery('#message-form').on('submit', function (e) {
     }, function () {
 
     });
-})
+});
+
+jQuery('[name=message]').keyup(function() {
+    socket.emit('getMessage', {
+        isTyping: true
+    });
+    setTimeout(function() {
+        socket.emit('getMessage', {
+            isTyping: false
+        });
+    }, 3000);
+});
+
+socket.on('setMessage', function(status){
+    if (status.isTyping) {
+        jQuery('#status').text('typing')
+    } else {
+        jQuery('#status').empty()
+    }
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.');
+    }
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function() {
+        alert('Unable to fetch location.');
+    });
+});
